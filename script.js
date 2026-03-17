8ccaf77d6d5ed5b990c17ff069cc0441a5458886
Ahelge <143036477+ajhelge@users.noreply.github.com>
2026-03-17 09:54:29 -0500

    Added simple mouse enter highlight effect to project cards

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function getProjects(){
+function getProjects() {
     window.location.href = "projects.html"
 }
-function getIndex(){
+function getIndex() {
     window.location.href = "index.html";
+}
+function highlight(id) {
+    document.getElementById(id).style.opacity = "80%";
+}
+function unHighlight(id) {
+    document.getElementById(id).style.opacity = "100%";
 }
