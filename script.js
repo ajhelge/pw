43d262b6f4884d7ad8b45e754ddd60dfbb440f55
Ahelge <143036477+ajhelge@users.noreply.github.com>
2026-03-26 21:30:58 -0500

    Created more generic getPage() fucntion

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-function getProjects() {
-    window.location.href = "projects.html"
-}
-function getIndex() {
-    window.location.href = "index.html";
+function getPage(id){
+    window.location.href = `${id}.html`
 }
 function highlight(id) {
+    console.log(`${id}`);
     document.getElementById(id).style.opacity = "80%";
 }
 function unHighlight(id) {
